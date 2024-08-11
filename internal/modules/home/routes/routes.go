b575c4b2a413f1ea95a158e8a4868539e735f8be
ahmadDev <ahmadjafari6050@gmail.com>
2024-08-11 12:22:43 +0330

    add controllers

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,18 +1,12 @@
 package routes
 
 import (
-	"blog/pkg/html"
-	"net/http"
+	homeCtrl "blog/internal/modules/home/controllers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/", func(c *gin.Context) {
-		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-			"title":   "Home page",
-			"appName": viper.Get("App.Name"),
-		})
-	})
+	homeController := homeCtrl.New()
+	router.GET("/", homeController.Index)
 }
