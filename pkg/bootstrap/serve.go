f272c10f4b5dcce2caa72f629a96e8ab0b9d6d65
ahmadDev <ahmadjafari6050@gmail.com>
2024-08-12 12:13:24 +0330

    connect to database

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"blog/pkg/config"
+	"blog/pkg/database"
 	"blog/pkg/html"
 	"blog/pkg/routing"
 	"blog/pkg/static"
@@ -9,6 +10,7 @@ import (
 
 func ServeApp() {
 	config.Set()
+	database.Connect()
 	routing.Init()
 	static.LoadStatic(routing.GetRouter())
 	html.LoadHTML(routing.GetRouter())
